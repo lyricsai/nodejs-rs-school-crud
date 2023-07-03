a006c53e50cd2771d3c1fa572b4f13a1fc0e6e57
Anastasiya Litvinenka <loffy@rambler.ru>
2023-07-04 01:24:51 +0300

    load balancer setup

--- src/lib/helpers/helpers.ts
+++ src/lib/helpers/helpers.ts
@@ -5,37 +5,39 @@ export const uuidLength = 36;
 export const startUuid = "/api/users/".length;
 
 export const idRegExp = new RegExp(
-    /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+  /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
 );
 
 export const checkValidId = (userId: string) => {
-    return !idRegExp.test(userId as string);
+  return !idRegExp.test(userId as string);
 };
 
 export const handleUserId = (res: ServerResponse, userId: string) => {
-    if (userId && (userId?.includes("/") || userId?.length !== uuidLength)) {
-        return handleError(404, res);
-    } else if (checkValidId(userId as string)) {
-        return handleError(400, res, "UserId is not valid");
-    }
+  if (userId && (userId?.includes("/") || userId?.length !== uuidLength)) {
+    return handleError(404, res);
+  } else if (checkValidId(userId as string)) {
+    return handleError(400, res, "UserId is not valid");
+  }
 };
 
 export const handleClusterRequest = (
-    req: IncomingMessage,
-    res: ServerResponse
+  req: IncomingMessage,
+  res: ServerResponse,
+  port,
+  workerIndex
 ) => {
-    const proxyReq = request({
-        host: "localhost",
-        // port: port,
-        method: req.method,
-        headers: req.headers,
-        path: req.url,
-    });
+  const proxyReq = request({
+    host: "localhost",
+    port: port + workerIndex + 1,
+    method: req.method,
+    headers: req.headers,
+    path: req.url,
+  });
 
-    proxyReq.on("response", (proxyRes: IncomingMessage) => {
-        res.writeHead(proxyRes.statusCode!, proxyRes.headers);
-        proxyRes.pipe(res);
-    });
+  proxyReq.on("response", (proxyRes: IncomingMessage) => {
+    res.writeHead(proxyRes.statusCode!, proxyRes.headers);
+    proxyRes.pipe(res);
+  });
 
-    req.pipe(proxyReq);
+  req.pipe(proxyReq);
 };
--- src/loadBalancer/loadBalncer.ts
+++ src/loadBalancer/loadBalncer.ts
@@ -1,69 +1,65 @@
-// import { IncomingMessage, ServerResponse, request } from "node:http";
+import http, { IncomingMessage, ServerResponse, request } from "http";
 // // import router from "../routes/index";
-// import { cpus } from "os";
-// import cluster from "cluster";
+import os from "os";
+import cluster from "cluster";
 // import { User } from "../models/user";
 // import { handleError } from "../services";
-// import { handleClusterRequest, balancing } from "../lib/helpers/helpers";
-// import server from "../index";
+import router from "../routes/index";
+import { handleClusterRequest } from "../lib/helpers/helpers";
+import "dotenv/config";
 
-// const users: User[] = [];
-// const cores = cpus();
+// import { UserService } from "../UserService.js";
 
 const loadBalancer = (port, instance) => {
-    //     try {
-    //         const workers = cores.map((_, i) => {
-    //             const workerPort = port + i + 1;
-    //             const worker = cluster.fork({ workerPort });
-    //             worker.on("message", (msg) => {
-    //                 balancing(msg);
-    //             });
-    //             return workerPort;
-    //         });
+  if (cluster.isPrimary) {
+    const numberParallelism = os.cpus().length;
+    const startPort = port;
+    const workers = [];
+    let nextPortIndex = 0;
+    const getnextPortIndex = () => {
+      nextPortIndex = (nextPortIndex % numberParallelism) + 1;
 
-    //         let portIndex = 0;
+      return nextPortIndex;
+    };
 
-    //         return handleClusterRequest(portIndex, workers);
-    //     } catch (error) {
-    //         console.log(error);
-    //         // return handleError(400, res, error.message);
-    //     }
-    console.log("No implementaion for load balancer");
+    const sendToAllWorkers = (users) => {
+      workers.forEach((worker) => {
+        (worker as any).send(users);
+      });
+    };
+
+    for (let index = 1; index <= numberParallelism; index += 1) {
+      const workerPort = startPort + index;
+      const worker = cluster.fork({ workerPort });
+      worker.on("message", (users) => {
+        sendToAllWorkers(users);
+      });
+      //@ts-ignore
+      workers.push(worker);
+    }
+
+    http
+      .createServer((req, res) => {
+        const workerIndex = getnextPortIndex();
+        handleClusterRequest(req, res, startPort, workerIndex);
+      })
+      .listen(startPort, () => {
+        `${instance} is running on the ${workers[nextPortIndex]} port`;
+      });
+  } else if (cluster.isWorker) {
+    const port = process.env["workerPort"];
+
+    process.on("message", (users) => {
+      // UserService.setUsers(users);
+      console.log(users);
+    });
+
+    http
+      .createServer((request, response) => {
+        router(request, response);
+      })
+      .listen(port);
+  }
 };
+
 export default loadBalancer;
-
-// import cluster from "cluster";
-// import http, { IncomingMessage, ServerResponse } from "http";
-// import net from "net";
-// import { handleClusterRequest } from "../lib/helpers/helpers";
-// import router from "../routes/index";
-
-// export const workers: http.Server[] = [];
-
-// export default function loadBalancer(processPort, instance) {
-//     let currentWorkerIndex = 0;
-//     const workerPorts = cores.map((_, i) => {
-//         const workerPort = processPort + i + 1;
-//         const worker = cluster.fork({ workerPort });
-//         worker.on("message", (msg) => {
-//             const method = msg.method;
-//             const args = msg.args || [];
-//             console.log(method, args);
-//         });
-//     });
-
-//     currentWorkerIndex = (currentWorkerIndex + 1) % workers.length;
-
-//     const serverMulti = http.createServer(handleClusterRequest);
-
-//     serverMulti.on("request", (req, res) => {
-//         router(req, res);
-//     });
-//     workers.push(serverMulti);
-
-//     serverMulti.listen(processPort, () => {
-//         const port = processPort;
-//         console.log(`${instance} ${process.pid} listening on port ${port}`);
-//     });
-
-// };
